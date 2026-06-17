2e28b736f3f960312396cdc98541f95352c68f36
DigitalDragon07 <nick.lahusen@gmail.com>
2026-06-17 12:29:01 +0200

    Updated api with new features

--- api/api.py
+++ api/api.py
@@ -1,6 +1,15 @@
 import urllib2
 import json
 
+userAgent = "TigerJython"
+
+def setUserAgent(name):
+    global userAgent
+    userAgent = nam
+    
+def getUserAgent():
+    return userAgent
+    
 class Patch(urllib2.Request):
     def get_method(self):
         return "PATCH"
@@ -25,13 +34,26 @@ class Post(urllib2.Request):
     def get_method(self):
         return "POST"
 
-def sendRequest(request, data = None, headers = None, prefferedMode = None):
+def sendRequest(mode, url, data = None, headers = None, prefferedMode = None):
+    global userAgent
+    if mode == "GET":
+        request = Get(url)
+    elif mode == "PATCH":
+        request = Patch(url, data)
+    elif mode == "PUT":
+        request = Put(url, data)
+    elif mode == "POST":
+        request = Post(url, data)
+    elif mode == "HEAD":
+        request = Head(url)
+    elif mode == "OPTIONS":
+        request = Options(url)
     if data != None:
         request.add_data(data)
     if headers != None:
         for i in headers:
             request.add_header(i, headers[i])
-    request.add_header("User-Agent", "TigerJython")
+    request.add_header("User-Agent", userAgent)
     f = urllib2.urlopen(request)
     if prefferedMode == None:
         return f
@@ -40,7 +62,7 @@ def sendRequest(request, data = None, headers = None, prefferedMode = None):
     elif prefferedMode == "info":
         return f.info()
         
-class Database():
+class Database(): #use a database with the free website JSONHosting (max 1 MB)
     allowModifikationNonexistantData = True
     @staticmethod
     def getData(url):
@@ -49,18 +71,18 @@ class Database():
         l = urllib2.urlopen(f)
         return l
     @staticmethod
-    def getOptions(url):
+    def getOptions(url): #equivalent to HTTP OPTIONS
         f = Options(url, None, {"User-Agent": "AndromedaOS"})
         l = urllib2.urlopen(f)
         return l
     @staticmethod
-    def setData(url, data, key):
+    def setData(url, data, key): #equivalent to HTTP POST
         data = json.dumps(data)
         f = Patch(url, data, {"User-Agent": "AndromedaOS", "X-Edit-Key": key, "Content-Type": "application/json"})
         l = urllib2.urlopen(f)
         return l
     @staticmethod
-    def setSpecificData(url, data, key):
+    def setSpecificData(url, data, key): #equivalent to HTTP PATCH
         data = json.dumps(data)
         oldData = json.loads(Database.getData(url))
         for i in data:
